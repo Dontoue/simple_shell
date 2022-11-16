99b2e0470400ec5f27f65f768df7360c55cd9fa9
Wachacha-jay <jameswachacha@gmail.com>
2022-11-16 18:39:58 +0000

    helper file

---
+++ free_double_ptr.c
@@ -0,0 +1,22 @@
+/*
+ * File: free_double_ptr.c
+ * Auth: Mwachacha-jay
+ * Dontoue
+ */
+#include "shell.h"
+
+/**
+ * free_double_ptr - free malloced arrays
+ * @str: array of strings
+ */
+void free_double_ptr(char **str)
+{
+	int i = 0;
+
+	while (str[i] != NULL)
+	{
+		free(str[i]);
+		i++;
+	}
+	free(str);
+}
